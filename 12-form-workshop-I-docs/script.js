d68c9d3797bb33eaaaa265f41b6c156ef1545792
bluebirds2920 <110462756+bluebirds2920@users.noreply.github.com>
2023-12-02 14:18:41 +0700

    finish laew na ja

--- 12-form-workshop-I-docs/script.js
+++ 12-form-workshop-I-docs/script.js
@@ -1,19 +1,42 @@
+
+
+
 (function () {
     'use strict'
-  
+
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.querySelectorAll('.needs-validation')
-  
+    
+   
+
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
       .forEach(function (form) {
         form.addEventListener('submit', function (event) {
+
+          
+          const TestNin =document.querySelector('#TestAlerter');
+
+
+
+
+
           if (!form.checkValidity()) {
             event.preventDefault()
             event.stopPropagation()
+            TestNin.classList.add('d-none');
+          }else{
+            event.preventDefault()
+            TestNin.classList.remove('d-none');
           }
   
           form.classList.add('was-validated')
         }, false)
       })
   })()
+
+  
+
+
+ 
